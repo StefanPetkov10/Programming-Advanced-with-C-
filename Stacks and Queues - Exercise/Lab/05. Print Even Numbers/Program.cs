07dbfbf1c8e035c03e1baca125d7c75aaab840d8
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2023-11-20 10:53:49 +0200

    Add Stacks and Queues tasks - Lab
    Problem Solutions Exercise: Advanced

---
+++ Stacks and Queues - Exercise/Lab/05. Print Even Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+
+namespace _05._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] digits = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> evenDigits = new Queue<int>();
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i] % 2 == 0)
+                {
+                    evenDigits.Enqueue(digits[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", evenDigits.ToArray()));
+            
+        }
+    }
+}
