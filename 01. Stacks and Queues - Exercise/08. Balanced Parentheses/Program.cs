e7e6aa6c20b449b177d0a871fd54e08a88895612
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2023-11-29 21:16:24 +0200

    Add Stacks and Queues tasks - Exercise
    Problem Solutions Exercise: Fundamentals

---
+++ 01. Stacks and Queues - Exercise/08. Balanced Parentheses/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Balanced_Parentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string parentheses = Console.ReadLine();
+
+            Stack<char> stack = new();
+
+            foreach (var parenthese in parentheses)
+            {
+                switch (parenthese)
+                {
+                    case '{':
+                    case '(':
+                    case '[':
+                        stack.Push(parenthese);
+                        break;
+                    case '}':
+                        if (stack.Count == 0 || stack.Pop() != '{')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                        break;
+                    case ')':
+                        if (stack.Count == 0 || stack.Pop() != '(')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                        break;
+                    case ']':
+                        if (stack.Count == 0 || stack.Pop() != '[')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                        break;
+                }
+            }
+
+            if (stack.Count > 0) // Missed case in judge ((((()))
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
