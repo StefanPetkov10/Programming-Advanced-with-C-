c7fb008723d89140901d35c4880dee027113b87c
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2023-12-15 20:07:11 +0200

    Add Sets and Dictionaries Advanced - Lab - More

---
+++ 03. Sets and Dictionaries Advanced - Exerxise/Lab/More/SortedSetDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedSetDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<int> sortedSet = new SortedSet<int>(new IntComparer());
+
+            sortedSet.Add(0);
+            sortedSet.Add(11);
+            sortedSet.Add(22);
+            sortedSet.Add(3);
+            sortedSet.Add(4);
+            sortedSet.Add(55);
+            sortedSet.Add(66);
+
+            foreach (var item in sortedSet)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+class IntComparer : IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                return y.CompareTo(x);
+            }
+        }
+    }
+}
+
