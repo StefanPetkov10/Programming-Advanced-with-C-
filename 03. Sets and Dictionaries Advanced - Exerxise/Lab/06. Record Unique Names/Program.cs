e09678f0f2565ace1a507a37f160eb6aabe4a0de
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2023-12-15 20:03:11 +0200

    Add Sets and Dictionaries Advanced - Lab

---
+++ 03. Sets and Dictionaries Advanced - Exerxise/Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            Dictionary<string, List<int>> dict;
+
+            names.OrderBy(x => x).ToHashSet();
+            for (int i = 0; i < n; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
