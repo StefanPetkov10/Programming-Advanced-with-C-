ecdf419536ed3f8eb4c53979675cb22706435fa8
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-01-22 23:26:41 +0200

    Add Functional Programming - Lab - More

---
+++ 05. Functional Programming - Exercise/Lab/More/Actions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Actions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int> printVar = Print;
+
+            printVar(5);
+
+            void Print(int x)
+            {
+                Console.WriteLine("Hello" + x);
+            }
+        }
+    }
+}
