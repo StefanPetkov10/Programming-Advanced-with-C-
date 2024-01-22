28ce3ab22b174507bdbfabac310f49e408ad02f6
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-01-23 01:27:11 +0200

    Add Functional Programming - Lab - More

---
+++ 05. Functional Programming - Exercise/Lab/More/Funcs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Funcs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, bool> isOddVariable = IsOdd;
+
+            bool isFiveOdd = isOddVariable(5);
+
+            bool IsOdd(int x)
+            {
+                return x % 2 == 0;
+            }
+        }
+    }
+}
