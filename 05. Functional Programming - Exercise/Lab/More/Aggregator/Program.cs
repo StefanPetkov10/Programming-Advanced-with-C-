10c6f1a50d2739a1abd4d20b6c7f955d2e1b8439
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-01-22 23:26:57 +0200

    Add Functional Programming - Lab - More

---
+++ 05. Functional Programming - Exercise/Lab/More/Aggregator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aggregator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Aggregate(1, 10, (x, y) => x + y));
+            Console.WriteLine(Aggregate(1, 10, (x, y) => x * y));
+            Console.WriteLine(Aggregate(1, 10, (x, y) => x / y));
+
+            long Aggregate(int start, int end, Func<long, long, long> op)
+            {
+                long result = start;
+                for (int i = start + 1; i <= end; i++)
+                    result = op(result, i);
+                return result;
+            }
+        }
+    }
+}
