14b37b62fa6a7eb9a3d8b14c8c70422192b29ada
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-01-23 01:27:24 +0200

    Add Functional Programming - Lab - More

---
+++ 05. Functional Programming - Exercise/Lab/More/Funcs2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Funcs2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, float, float> addFunc = Add;
+
+            float Add(int a, float b)
+            {
+
+                return a + b;
+            }
+        }
+    }
+}
