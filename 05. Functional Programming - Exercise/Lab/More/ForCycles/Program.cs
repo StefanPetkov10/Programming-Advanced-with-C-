42d3be438ef684bd6e8f705fb35bbcc4df242e66
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-01-23 01:26:58 +0200

    Add Functional Programming - Lab - More

---
+++ 05. Functional Programming - Exercise/Lab/More/ForCycles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ForCycles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Hello");
+            //}
+
+            Print(10);
+
+            void Print(int times)
+            {
+
+                if (times == 0)
+                {
+                    return;
+                }
+                Console.WriteLine("Hello");
+
+                Print(times - 1);
+            }
+        }
+    }
+}
