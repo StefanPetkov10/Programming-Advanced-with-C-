042ffb267ac9a73c78381184f31afe3747f0c8ed
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-01-22 23:27:20 +0200

    Add Functional Programming - Lab - More

---
+++ 05. Functional Programming - Exercise/Lab/More/DataImmutability/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DataImmutability
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Add(int a, int b)
+            {
+                a = 5;
+                a++;
+                int n = 10;
+                n = 3;
+
+                return a + b;
+            }
+        }
+    }
+}
