142ef17d67780fb8d038bacfb74ac14f4d0e3913
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-01-23 01:26:45 +0200

    Add Functional Programming - Lab - More

---
+++ 05. Functional Programming - Exercise/Lab/More/FirstClassFunctions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FirstClassFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var functionVariable = Add;
+
+            //Console.WriteLine(functionVariable(5, 6));
+            //Console.WriteLine(functionVariable(7, 8));
+
+            //int Add(int a, int b)
+            //{
+            //    return int Divide(int x, int y){
+            //        return x / y;
+            //    };
+            //}
+        }
+    }
+}
