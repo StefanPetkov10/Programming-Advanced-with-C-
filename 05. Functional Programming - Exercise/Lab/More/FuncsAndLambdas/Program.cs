3b91f31c400ca0a7f43bc3e5a584385e62a9aecf
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-01-24 13:23:33 +0200

    Add Functional Programming - Lab - More

---
+++ 05. Functional Programming - Exercise/Lab/More/FuncsAndLambdas/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FuncsAndLambdas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4 };
+
+
+            Func<string, int, float, int> isOdd = (string s, int x, float y) => x % 2;
+        }
+    }
+}
