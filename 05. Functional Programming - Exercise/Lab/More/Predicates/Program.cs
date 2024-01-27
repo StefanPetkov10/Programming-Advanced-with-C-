09e1b58824fdf5cbe1b4bb3ca3abd69b5fd692c3
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-01-27 19:58:12 +0200

    Add Functional Programming - Lab - More

---
+++ 05. Functional Programming - Exercise/Lab/More/Predicates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Predicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, bool> filter = IsOdd;
+
+            Predicate<int> predicate = IsOdd;
+
+            //new List<int>().Where()
+
+
+            bool IsOdd(int x)
+            {
+                return x % 2 == 0;
+            }
+        }
+    }
+}
