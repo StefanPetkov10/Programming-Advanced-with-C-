a638b900ca9fdfd9d591d47b825a49b3cb6908b3
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-02-05 18:57:43 +0200

    Add Lab Defining Classes - Lab

---
+++ 06. Lab Defining Classes - Exercise/Lab/CarManufacturer/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car3 = new Car();
+
+            Car car2 = new Car("BMW", "X3", 2006);
+
+            Car car = new Car("BMW", "X3", 2006, 500, 300);
+
+            car.Drive(5);
+
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
