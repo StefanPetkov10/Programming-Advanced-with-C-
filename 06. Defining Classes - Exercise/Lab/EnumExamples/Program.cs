da6ce874f24b2726235c672634743b2646b88e9c
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-02-06 19:21:43 +0200

    Add Defining Classes - Lab

---
+++ 06. Defining Classes - Exercise/Lab/EnumExamples/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using EnumExamples;
+using System.Numerics;
+Player player = new Player();
+
+while (true)
+{
+    if (player.Direction == Direction.Right)
+    {
+        Console.WriteLine();
+    }
+    if (player.Direction == Direction.Left)
+    {
+
+    }
+    if (player.Direction == Direction.Up)
+    {
+
+    }
+
+}
