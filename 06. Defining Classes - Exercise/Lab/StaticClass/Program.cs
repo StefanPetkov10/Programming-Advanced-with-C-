a4c4b695c4428a3e98ac4ef7b4b09032374d6338
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-02-09 19:45:05 +0200

    Add Defining Classes - Lab

---
+++ 06. Defining Classes - Exercise/Lab/StaticClass/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Microsoft.VisualBasic;
+using StaticClass;
+Console.WriteLine();
+Constants.PrintMessage();
+
+//Math.PI;
+
+String.IsNullOrEmpty("");
+
+DateTime.Now;
+
+Array.Sort(new int[] { });
