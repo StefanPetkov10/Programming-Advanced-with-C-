6a83bc3aa52865f9a3477598215444b9ae1f01d9
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-03-19 22:16:36 +0200

    Add task - Mail Client

---
+++ 12. Regular Exam/C# Advanced Regular Exam - 21 October 2023/03.Mail Client/Mail.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace MailClient
+{
+    public class Mail
+    {
+        public Mail(string sender, string receiver, string body)
+        {
+            Sender = sender;
+            Receiver = receiver;
+            Body = body;
+        }
+
+        public string Sender  { get; set; }
+        public string Receiver  { get; set; }
+        public string Body { get; set; }
+
+        public override string ToString() => $"From: {Sender} / To: {Receiver}{Environment.NewLine}Message: {Body}";
+    }
+}
---
+++ 12. Regular Exam/C# Advanced Regular Exam - 21 October 2023/03.Mail Client/MailBox.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MailClient
+{
+    public class MailBox
+    {
+        public MailBox(int capacity)
+        {
+            Capacity = capacity;
+            Archive = new List<Mail>();
+            Inbox = new List<Mail>();
+        }
+
+        public int Capacity { get; set; }
+        public List<Mail> Archive { get; set; }
+        public List<Mail> Inbox { get; set; }
+
+        public void IncomingMail(Mail mailbox)
+        {
+            if (Inbox.Count < Capacity)
+            {
+                Inbox.Add(mailbox);
+            }
+        }
+        //public bool DeleteMail(string sender)
+        //{
+        //    Mail mail = Inbox.FirstOrDefault(s => s.Sender == sender);
+        //    if (mail != null)
+        //    {
+        //        Inbox.Remove(mail);
+        //        return true;
+        //    }
+        //    else return false;
+        //}
+        public bool DeleteMail(string sender)
+    => Inbox.Remove(Inbox.FirstOrDefault(m => m.Sender == sender));
+        public int ArchiveInboxMessages()
+        {
+            int count = Inbox.Count;
+            Archive.AddRange(Inbox);
+            Inbox = new List<Mail>();
+
+            return count;
+        }
+
+
+        //public string GetLongestMessage() => Inbox.MaxBy(c => c.Body.Length).ToString();
+        public string GetLongestMessage()
+          => Inbox.OrderByDescending(m => m.Body.Length).FirstOrDefault().ToString();
+
+        public string InboxView()
+        {
+
+            StringBuilder sb = new();
+            sb.AppendLine("Inbox:");
+
+            foreach (Mail mail in Inbox)
+            {
+                sb.AppendLine(mail.ToString().TrimEnd());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
